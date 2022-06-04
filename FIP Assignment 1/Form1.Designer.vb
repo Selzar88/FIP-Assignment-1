c0b709281ddddc0380ded7cfb54b1991622fe5d8
wisni <wisni@LAPTOP-7AN5K9RK>
2022-06-04 14:11:17 +0100

    Add project files.

---
+++ FIP Assignment 1/Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CVV = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ValBt = New System.Windows.Forms.Button()
+        Me.CardNo = New System.Windows.Forms.TextBox()
+        Me.ExDate = New System.Windows.Forms.DateTimePicker()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(72, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(99, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Credit Card Number"
+        '
+        'CVV
+        '
+        Me.CVV.Location = New System.Drawing.Point(224, 54)
+        Me.CVV.Name = "CVV"
+        Me.CVV.Size = New System.Drawing.Size(36, 20)
+        Me.CVV.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(232, 38)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(28, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "CVV"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(72, 89)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Expire date"
+        '
+        'ValBt
+        '
+        Me.ValBt.BackColor = System.Drawing.Color.PowderBlue
+        Me.ValBt.Location = New System.Drawing.Point(91, 143)
+        Me.ValBt.Name = "ValBt"
+        Me.ValBt.Size = New System.Drawing.Size(156, 66)
+        Me.ValBt.TabIndex = 7
+        Me.ValBt.Text = "Click to validate"
+        Me.ValBt.UseVisualStyleBackColor = False
+        '
+        'CardNo
+        '
+        Me.CardNo.Location = New System.Drawing.Point(73, 54)
+        Me.CardNo.MaxLength = 14141
+        Me.CardNo.Name = "CardNo"
+        Me.CardNo.Size = New System.Drawing.Size(114, 20)
+        Me.CardNo.TabIndex = 8
+        '
+        'ExDate
+        '
+        Me.ExDate.Location = New System.Drawing.Point(73, 105)
+        Me.ExDate.Name = "ExDate"
+        Me.ExDate.Size = New System.Drawing.Size(155, 20)
+        Me.ExDate.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightSteelBlue
+        Me.ClientSize = New System.Drawing.Size(336, 259)
+        Me.Controls.Add(Me.CardNo)
+        Me.Controls.Add(Me.ValBt)
+        Me.Controls.Add(Me.ExDate)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.CVV)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Credit Card Validator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CVV As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ValBt As Button
+    Friend WithEvents CardNo As TextBox
+    Friend WithEvents ExDate As DateTimePicker
+End Class
---
+++ FIP Assignment 1/Form1.vb
@@ -0,0 +1,54 @@
+﻿Public Class Form1
+    'Luhn algorithm
+    Private Function CardValidator(ByVal value As String) As Boolean
+        Dim checksum As Integer = 0 'This will store sum of the digits.
+        Dim doubleflag As Boolean = (value.Length Mod 2 = 0) 'Loop will pick every alternate digit starting from the 2nd from left.
+        Dim digit As Char 'Digit stored as a character (onr character at the time)
+        Dim digitvalue As Integer 'Digitvalue stored in memory as an integer.
+        For Each digit In value 'Each alternate digit will be doubled.
+            digitvalue = Integer.Parse(digit) 'String converted to an Integer.
+            If doubleflag Then 
+                digitvalue *= 2
+                If digitvalue > 9 Then
+                    digitvalue -= 9 'If the doubled result is greater then 9 program will substract 9 from the result.
+                End If
+            End If
+            'Adding all of the digits, doubled and not.
+            checksum += digitvalue 'adding the dubled value to the existing value
+            doubleflag = Not doubleflag 'toggle the value of variable
+        Next
+        Return (checksum Mod 10 = 0) 'Verification If final result is divisible by 10 without a reminder.
+    End Function
+    Private Sub ValBt_click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ValBt.Click
+        'Pop up windows with alert for the wrong length of the Card Number.
+        If CardNo.Text.Length <> 16 Then
+            MsgBox("Invalid Card Number. Please provide 16 digits")
+            'Pop-up windows with alert for the worng length of the CVV Number.
+        ElseIf CVV.Text.Length <> 3 Then
+            MsgBox("CVV must contain 3 digits")
+            'If above conditions are met then program continue to the validation process.
+        ElseIf CardValidator(CardNo.Text) Then
+            MsgBox("Card is Valid") 'Pop-up window with Valid message if the sum of the digits is divisible by 10 without a reminder.
+        Else
+            MsgBox("Card is not valid") 'Pop-up window with invalid message If the division of the sum of the digits have reminder.
+        End If
+       End Sub
+
+    Private Sub CardNo_TextChanged(sender As Object, e As EventArgs) Handles CardNo.TextChanged
+        'This function will restrict input to digits only in the Card Number textbox.
+        If Not IsNumeric(CardNo.Text) Then
+            MsgBox("Please enter digits only.")
+        End If
+    End Sub
+
+    Private Sub CVV_TextChanged(sender As Object, e As EventArgs) Handles CVV.TextChanged
+        'This function will restrict input to digits only in the CVV textbox.
+        If Not IsNumeric(CVV.Text) Then
+            MsgBox("Please enter digits only.")
+        End If
+    End Sub
+    'This function will not allow to pick up past date. 
+    Private Sub ExDate_ValueChanged(sender As Object, e As EventArgs) Handles ExDate.ValueChanged
+        ExDate.MinDate = Date.Today
+    End Sub
+End Class
